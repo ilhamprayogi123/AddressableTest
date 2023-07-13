0bda4f693ff349d5ce1f3d9a3c4424f6a166c2b3
ilhamprayogi123 <ilhamprayogi17@gmail.com>
2023-07-13 10:49:53 +0700

    Test Upload
    Testing upload

---
+++ Assets/Script/AddressableManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AddressableAssets;
+using Cinemachine;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class AddressableManager : MonoBehaviour
+{
+    [SerializeField]
+    private AssetReference playerArmatureAssetReference;
+
+    //[SerializeField]
+    //private AssetReferenceTexture2D unityLogoAssetReference;
+
+    [SerializeField]
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+
+    private GameObject playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Addressables.InitializeAsync().Completed += AddressableManager_Completed;
+    }
+
+    private void AddressableManager_Completed(AsyncOperationHandle<IResourceLocator> obj)
+    {
+        playerArmatureAssetReference.InstantiateAsync().Completed += (go) =>
+        {
+            playerController = go.Result;
+            cinemachineVirtualCamera.Follow = playerController.transform.Find("PlayerCameraRoot");
+        };
+    }
+
+    private void OnDestroy()
+    {
+        playerArmatureAssetReference.ReleaseInstance(playerController);
+    }
+}
